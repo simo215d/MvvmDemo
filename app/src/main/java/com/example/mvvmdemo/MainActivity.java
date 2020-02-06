593fbefd77471163e20cf8107b9af92f5cc227ab
simo215d <simo215d@edu.easj.dk>
2020-02-06 11:34:03 +0100

    Når knappen klikkes bliver textviewets text sat til edittexts text.

--- app/src/main/java/com/example/mvvmdemo/MainActivity.java
+++ app/src/main/java/com/example/mvvmdemo/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.mvvmdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //find the views
+        button = findViewById(R.id.button1);
+        textView = findViewById(R.id.text_view1);
+        editText = findViewById(R.id.edit_text1);
+
+        //add a click listener and specify the click event
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(editText.getText());
+            }
+        });
     }
 }
